d9e883470872be8784ee4bb2242a65314b09d713
Arthur Niedzwiecki <artnie91@fb3.uni-bremen.de>
2016-10-26 19:41:45 +0200

    :unicorn: init pepper listener

--- pepper_communication/lisp/command-listener.lisp
+++ pepper_communication/lisp/command-listener.lisp
@@ -1,4 +1,5 @@
 (in-package :pepper-communication-package)
 
 (defun listen-to-pepper ()
-  "This is the service to listen to pepper.")
+  "This is the service to listen to pepper."
+  (execute-pepper-command))
