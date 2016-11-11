35efa387ec0f5efdc149d5703b636d2f3ce73056
Christopher Pollok <cpo@fb3.uni-bremen.de>
2016-11-12 00:52:30 +0100

    :muscle: Replace PriorityQueue with heapq functions

--- prototype/planner_base.py
+++ prototype/planner_base.py
@@ -1,5 +1,6 @@
 from operator_base import *
 from predicate_evaluator import *
+import heapq
 
 try:
     from Queue import PriorityQueue  # ver. < 3.0
@@ -85,7 +86,8 @@ class Planner(object):
         self.goal = None
         self.predBase = pred_base
         self.opBase = op_base
-        self.problemHeap = PriorityQueue()
+        # self.problemHeap = PriorityQueue()
+        self.problemHeap = []
 
     def generate_permutations(self, op, diff):
         opts = [None] * len(op.b_inst)
@@ -179,7 +181,8 @@ class Planner(object):
                                                  problem.planL,
                                                  Plan(op, tail=problem.planR))
                 if new_problem is not None:
-                    self.problemHeap.put(new_problem)
+                    # self.problemHeap.put(new_problem)
+                    heapq.heappush(self.problemHeap, new_problem)
             else:
                 plan = Plan(op, head=problem.planL, tail=problem.planR)
                 while plan.head is not None:
@@ -190,7 +193,8 @@ class Planner(object):
                 if ok:
                     return plan
                 else:
-                    self.problemHeap.put(new_problem)
+                    # self.problemHeap.put(new_problem)
+                    heapq.heappush(self.problemHeap, new_problem)
 
         return None
 
@@ -200,7 +204,8 @@ class Planner(object):
         self.start = start
         self.goal = goal
         if initial_problem is not None:
-            self.problemHeap.put(initial_problem)
+            # self.problemHeap.put(initial_problem)
+            heapq.heappush(self.problemHeap, initial_problem)
             return True
         else:
             return False
@@ -208,7 +213,8 @@ class Planner(object):
     def get_next_plan(self):
         plan = None
         # while plan == None and not self.problemHeap.empty():
-        next_problem = self.problemHeap.get()
+        # next_problem = self.problemHeap.get()
+        next_problem = heapq.heappop(self.problemHeap)
         print(next_problem)
         plan = self.solve_problem(next_problem)
 
