a2c4371ebdfa66226515dd0069c4756a624532c5
Arthur Niedzwiecki <artnie91@fb3.uni-bremen.de>
2016-11-11 13:56:49 +0100

    :bug: adding missing service call

--- pr2_command_pool/lisp/services.lisp
+++ pr2_command_pool/lisp/services.lisp
@@ -2,3 +2,6 @@
 
 (defun service-get-object-pose (object-id)
   "call prolog service to receive the pose (including the location) of the object with given id")
+
+(defun service-get-side-location (side)
+  "call prolog service to get location of given side, left or right")
