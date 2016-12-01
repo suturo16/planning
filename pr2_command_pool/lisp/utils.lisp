cee7fcc9544fdb1d273a44658937bf88cebfdf61
Christopher Pollok <cpollok@uni-bremen.de>
2016-12-01 16:23:08 +0100

    :unicorn: Add a split function and a function to get a (semi-hardcoded) path of a yaml file.

--- pr2_command_pool/lisp/utils.lisp
+++ pr2_command_pool/lisp/utils.lisp
@@ -8,3 +8,22 @@
             while line do (setf out (concatenate 'string out line (string #\linefeed))))
       (close in))
     out))
+
+(defun split (string &key (delimiterp #'delimiterp))
+  (loop
+    :for beg = (position-if-not delimiterp string)
+    :then (position-if-not delimiterp string :start (1+ end))
+    :for end = (and beg (position-if delimiterp string :start beg))
+    :when beg :collect (subseq string beg end)
+    :while end))
+
+(defun delimiterp (c) (position c "/"))
+
+; TODO(cpo): Make more stable, only works for systems on root level in planning repo
+(defun get-controller-yaml-path ()
+  (let ((path (sb-unix:posix-getcwd)))
+    (concatenate 'string
+                 (subseq path
+                         0
+                         (position "/" path :from-end t :test #'string-equal))
+                 "/yaml/controller_specs.yml")))
