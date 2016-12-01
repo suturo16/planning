64ce2e0c092438258934bc00bfb4c08e422a8ea4
Christopher Pollok <cpollok@uni-bremen.de>
2016-12-01 20:10:23 +0100

    :muscle: Improve yaml path functions. Add function to get joints as a list from file.

--- pr2_command_pool/lisp/utils.lisp
+++ pr2_command_pool/lisp/utils.lisp
@@ -17,13 +17,35 @@
     :when beg :collect (subseq string beg end)
     :while end))
 
-(defun delimiterp (c) (position c "/"))
+(defun delimiterp (c) (position c ":"))
+
+(defun get-joints (config-name)
+  (let ((in (open (get-config-yaml-path config-name) :if-does-not-exist nil))
+        (out nil))
+    (when in
+      (loop for line = (read-line in nil)
+            while line do (setf out (cons (subseq line 2) out)))
+      (close in))
+    (reverse out)))
+
+(defun get-controller-yaml-path (controller-name)
+  (concatenate 'string
+               (get-yaml-path "controller_specs")
+               controller-name))
+
+(defun get-config-yaml-path (config-name)
+  (concatenate 'string
+               (get-yaml-path "config")
+               config-name))
 
 ; TODO(cpo): Make more stable, only works for systems on root level in planning repo
-(defun get-controller-yaml-path ()
+(defun get-yaml-path (type)
   (let ((path (sb-unix:posix-getcwd)))
     (concatenate 'string
                  (subseq path
                          0
                          (position "/" path :from-end t :test #'string-equal))
-                 "/yaml/controller_specs.yml")))
+                 "/yaml/"
+                 type
+                 "/")))
+  
