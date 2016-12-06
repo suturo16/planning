b32801181b8dea97caa230062e1466a651d790f1
hasu <hawkin@informatik.uni-bremen.de>
2016-12-06 20:50:13 +0100

    :muscle: fixing a mistake with lists

--- plan_execution/lisp/toplevel.lisp
+++ plan_execution/lisp/toplevel.lisp
@@ -3,11 +3,11 @@
 (defun execute (plan)
   "Start toplevel plan here."
   (print plan)
-  (if (string= (car plan) "pick-up-object")
+  (if (string= (setq plan (car plan)) "pick-up-object")
       (pick-up-object "zylinder" "right")
       (ros-info (plan-execution-system) "First plan was not pick-up-object. Won't do anything then.")
       )
-   (if (string= (cdr plan) "put-down-object")
+  (if (string= (setq plan (car plan)) "put-down-object")
       (put-down-object "right" "right")
       (ros-info (plan-execution-system) "Second plan was not  put-down -object. Won't do anything then.")
       )
