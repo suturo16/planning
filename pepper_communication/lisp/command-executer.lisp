f1263979dda81b7df0a851930394a3a8e9658a4c
hasu <hawkin@informatik.uni-bremen.de>
2016-12-06 20:35:56 +0100

    :muscle: fixing a few lines

--- pepper_communication/lisp/command-executer.lisp
+++ pepper_communication/lisp/command-executer.lisp
@@ -4,7 +4,7 @@
 ;we assume that "command" currently contains only the direction the object should be put down to. aka: right or left (ms1)
 (defun execute-pepper-command (command)
   "Here the command retrieved from the pepper-listener should be executed."
-  (list "pick-up-object" "put-down-object" command)
-  (plan-execution-package::execute command)  
-  (print command))
+  (print "this is the given command: " command)
+  (pexecution::execute (list "pick-up-object" "put-down-object" command))  
+ )
 
