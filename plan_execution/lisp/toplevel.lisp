e1a2732fc84d7e7ebf02af0f5ad456f4d731a934
hasu <hawkin@informatik.uni-bremen.de>
2016-12-06 22:41:44 +0100

    :bug: fixed the invalid argument issue

--- plan_execution/lisp/toplevel.lisp
+++ plan_execution/lisp/toplevel.lisp
@@ -3,13 +3,13 @@
 (defun execute (plan)
   "Start toplevel plan here."
   (print plan)
-  (if (string= (setq plan (car plan)) "pick-up-object")
+  (if (string= (first plan) "pick-up-object")
       (pick-up-object "zylinder" "right")
       (ros-info (plan-execution-system) "First plan was not pick-up-object. Won't do anything then.")
       )
-  (if (string= (setq plan (car plan)) "put-down-object")
+  (if (string= (second plan) "put-down-object")
       (put-down-object "right" "right")
-      (ros-info (plan-execution-system) "Second plan was not  put-down -object. Won't do anything then.")
+      (ros-info (plan-execution-system) "Second plan was not  put-down-object. Won't do anything then.")
       )
 
   )
