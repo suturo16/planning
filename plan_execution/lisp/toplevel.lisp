d64d4f9b1ffd55df6623e03b31ff0ae934a94775
hasu <hawkin@informatik.uni-bremen.de>
2016-12-07 15:45:05 +0100

    :muscle: making code lispy pretty

--- plan_execution/lisp/toplevel.lisp
+++ plan_execution/lisp/toplevel.lisp
@@ -4,12 +4,8 @@
   "Start toplevel plan here."
   (print plan)
   (if (string= (first plan) "pick-up-object")
-      (pick-up-object "zylinder" "right")
-      (ros-info (plan-execution-system) "First plan was not pick-up-object. Won't do anything then.")
-      )
+      (pick-up-object "cylinder" (third plan))
+      (ros-info (plan-execution-system) "First plan was not pick-up-object. Won't do anything then."))
   (if (string= (second plan) "put-down-object")
-      (put-down-object "right" "right")
-      (ros-info (plan-execution-system) "Second plan was not  put-down-object. Won't do anything then.")
-      )
-
-  )
+      (put-down-object "right" (third plan))
+      (ros-info (plan-execution-system) "Second plan was not  put-down-object. Won't do anything then.")))
