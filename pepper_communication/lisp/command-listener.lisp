037787dcab05cb5c1a7e885e0062860d36c9ffc7
hasu <hawkin@informatik.uni-bremen.de>
2016-12-07 17:09:58 +0100

    :bug: fixed a print bug in the pepper-command-listener

--- pepper_communication/lisp/command-listener.lisp
+++ pepper_communication/lisp/command-listener.lisp
@@ -1,6 +1,9 @@
 (in-package :pepper-communication-package)
 
-(defun listen-to-pepper ()
-  "This is the service to listen to pepper."
-  (with-ros-node ("pepper_command_listener" :spin t)
-    (subscribe "pepper_command" "std_msgs/String" #'execute-pepper-command)))
+;execute-command-from-pepper
+;we assume that "command" currently contains only the direction the object should be put down to. aka: right or left (ms1)
+(defun execute-pepper-command (command)
+  "Here the command retrieved from the pepper-listener should be executed."
+  (with-fields ((command-string (data))) command
+    (print command-string)
+      (pexecution::execute (list "pick-up-object" "put-down-object" command-string))))
