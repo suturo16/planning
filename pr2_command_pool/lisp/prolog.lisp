36d8f28ede27f8e23dbd7f261fed359fdd2570b2
Arthur Niedzwiecki <arthur.niedzwiecki@gmail.com>
2016-12-08 18:04:14 +0100

    :feature: Add prolog call to get object pose.

--- pr2_command_pool/lisp/prolog.lisp
+++ pr2_command_pool/lisp/prolog.lisp
@@ -1,1 +1,6 @@
 (in-package :pr2-command-pool-package)
+
+(defun prolog-get-object-pose (type)
+  "Returns the pose of the object with given id as PoseStamped."
+  (cram-utilities:force-ll (json-prolog::prolog `(getObjectPose ,type ?pose))))
+
