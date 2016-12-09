1f693b767c20e5d30645e54e06f643965f511ab9
Christopher Pollok <cpollok@uni-bremen.de>
2016-12-09 14:40:22 +0100

    :unicorn: Add mockup for place_action server

--- sut_mockups/scripts/graspkard.py
+++ sut_mockups/scripts/graspkard.py
@@ -25,7 +25,27 @@ class MoveRobotServer(TemplateActionServer):
             r.sleep()
 
 
+class PlaceObjectServer(TemplateActionServer):
+    def __init__(self):
+        super(PlaceObjectServer, self).__init__("~" + "place_object", MoveRobotAction)
+
+    def execute(self, goal):
+        r = rospy.Rate(1)
+        feedback = MoveRobotFeedback()
+        feedback.current_value = 10
+        feedback.alteration_rate = 1
+        while True:
+            if self.server.is_preempt_requested():
+                self.server.set_preempted()
+                break
+            if feedback.current_value > 0:
+                feedback.current_value -= 1
+            self.server.publish_feedback(feedback)
+            r.sleep()
+
+
 if __name__ == '__main__':
     rospy.init_node(constants.graspkard, anonymous=True)
     server = MoveRobotServer()
+    server = PlaceObjectServer()
     rospy.spin()
