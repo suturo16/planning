7d3f7f3f9f1207e266326be9b8496206620db191
Christopher Pollok <cpollok@uni-bremen.de>
2017-01-06 15:14:38 +0100

    :unicorn: Add simple mockup for simple logger.

---
+++ sut_mockups/scripts/simple_logger.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import rospy
+import constants
+from template import TemplateService
+from suturo_knowledge_msgs.srv import LogAction, LogActionResponse, LogExperimentDescription, LogExperimentDescriptionResponse
+
+if __name__ == '__main__':
+    rospy.init_node('template')
+    service = TemplateService(constants.log_action, LogAction, LogActionResponse)
+    service = TemplateService(constants.log_experiment_description, LogExperimentDescription, LogExperimentDescriptionResponse)
+    rospy.spin()
