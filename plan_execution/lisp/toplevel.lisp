f177bcabd8243d0529f8517f9feab6da3c12e63a
hasu <hawkin@informatik.uni-bremen.de>
2017-02-09 19:35:07 +0100

    :fire: removing unecessary print outs

--- plan_execution/lisp/toplevel.lisp
+++ plan_execution/lisp/toplevel.lisp
@@ -1,16 +1,8 @@
 (in-package :plan-execution-package)
 
-;"in order to prevent the plan from starting again when it is already running
-;(let ((executing (make-fluent :name "executing" :value nil))))
-(defvar *executing* (make-fluent :name "executing" :value nil))
-(defvar *plans-list* nil)
-
-;idea: have a q of plans
 (cram-language:def-top-level-cram-function execute (plan)  
   (execute-cram plan))
     
-
-
 ;chris stuff
 (cram-language:def-cram-function execute-cram (task)
   (when (eq (node-status) :SHUTDOWN)
@@ -24,15 +16,11 @@
     (pm-execute :manipulation (car desigs))
     (execute-desigs (cdr desigs))))
 
-(defun task->designators (task)
+(defun task->designators (task)  
   (ecase task
     ('grasp-cylinder
-     (print "grasp")
-     (list
-      (make-designator :action `((:type :grasp) (:arm ,pr2-do::+right-arm+) (:object "cylinder")))))
+     (list (make-designator :action `((:type :grasp) (:arm ,pr2-do::+right-arm+) (:object "cylinder")))))
     ('cut
-     (print "cut")
-     (list
-      (make-designator :action `((:type :grasp) (:arm ,pr2-do::+right-arm+) (:object "knife")))
-      (make-designator :action `((:type :cut) (:arm ,pr2-do::+right-arm+) (:knife "knife") (:cake "cake")))))))
+     (list (make-designator :action `((:type :grasp) (:arm ,pr2-do::+right-arm+) (:object "knife")))
+           (make-designator :action `((:type :cut) (:arm ,pr2-do::+right-arm+) (:knife "knife") (:cake "cake")))))))
 
