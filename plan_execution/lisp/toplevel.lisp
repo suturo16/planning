9f943565c85fd710106c5d753bad5b93dcb2ff72
hasu <hawkin@informatik.uni-bremen.de>
2017-02-09 19:16:33 +0100

    :burn: removing unecessary stuffs

--- plan_execution/lisp/toplevel.lisp
+++ plan_execution/lisp/toplevel.lisp
@@ -6,40 +6,18 @@
 (defvar *plans-list* nil)
 
 ;idea: have a q of plans
-(defun execute (plan)
-  (push plan *plans-list*)
-  
-  (print "wait-for-executing...")
-    (wait-for *executing*)
-
-  ;checks first if a fucntion with a matching name exists, then tries to call it.
-   (progn
-     (if (fboundp (intern (string-upcase (car (last *plans-list*))) 'plan-execution-package))
-         (progn
-           (funcall (intern (string-upcase (car (last *plans-list*))) 'plan-execution-package))))
-           (print "This function is not defined, or the plan doesn't exist.")))
-
-
-;fucntions for testing stuff
-;this currently has to be the top lvl function, because one cannot nest these.
-(def-top-level-cram-function testy ()
-  (setf (value *executing*) NIL)
-  (par
-   (progn
-     (execute "test-function"))
-   (progn
-     (sleep 5)
-     (setf (value *executing*) T))))
-
+(cram-language:def-top-level-cram-function execute (plan)  
+  (execute-cram plan))
+    
 
 
 ;chris stuff
-(cram-language:def-top-level-cram-function execute-cram (task)
+(cram-language:def-cram-function execute-cram (task)
   (when (eq (node-status) :SHUTDOWN)
     (start-ros-node "planning"))
   (with-pr2-process-modules
     (process-module-alias :manipulation 'giskard-manipulation)
-    (execute-desigs (task->designators task))))
+    (execute-desigs (intern (string-upcase (task->designators task))))))
 
 (defun execute-desigs (desigs)
   (when desigs
@@ -48,10 +26,13 @@
 
 (defun task->designators (task)
   (ecase task
-    (:grasp-cylinder
+    ('grasp-cylinder
+     (print "grasp")
      (list
       (make-designator :action `((:type :grasp) (:arm ,pr2-do::+right-arm+) (:object "cylinder")))))
-    (:cut
+    ('cut
+     (print "cut")
      (list
       (make-designator :action `((:type :grasp) (:arm ,pr2-do::+right-arm+) (:object "knife")))
       (make-designator :action `((:type :cut) (:arm ,pr2-do::+right-arm+) (:knife "knife") (:cake "cake")))))))
+
