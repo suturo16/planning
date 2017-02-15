55a7853e195329649b816c1de1fded4181821eee
hasu <hawkin@informatik.uni-bremen.de>
2017-02-15 14:24:01 +0100

    :feature: added notify for pepper after pr2 is done, fixed a typo

--- pepper_communication/lisp/command-executer.lisp
+++ pepper_communication/lisp/command-executer.lisp
@@ -2,5 +2,8 @@
 
 (defun execute-pepper-command (command)
   "Execute the given command."
-  (plan-execution-package::execute command))
+  (plan-execution-package::execute command)
+  ;command should be called "notify" but hasn't been updated yet.
+  ;the IP of Pepper: "192.168.101.96"
+  (fire-rpc "setStatus" "127.0.0.1" 8000))
 
