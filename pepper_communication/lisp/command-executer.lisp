a58b76041daab7e2a85c064ff96f5b956289ca97
Arthur Niedzwiecki <arthur.niedzwiecki@gmail.com>
2017-02-15 19:02:46 +0100

    :muscle: Improve pepper notify.

--- pepper_communication/lisp/command-executer.lisp
+++ pepper_communication/lisp/command-executer.lisp
@@ -3,7 +3,10 @@
 (defun execute-pepper-command (command)
   "Execute the given command."
   (plan-execution-package::execute command)
-  ;command should be called "notify" but hasn't been updated yet.
-  ;the IP of Pepper: "192.168.101.96"
-  (fire-rpc "setStatus" "192.168.101.97" 8000))
+
+  (if (gethash :pepper *clients*)
+      (fire-rpc-to-client "notify"
+                          (client-host (gethash :pepper *clients*))
+                          (client-port (gethash :pepper *clients*)))
+      (fire-rpc "notify" "192.168.101.97" 8000)))
 
