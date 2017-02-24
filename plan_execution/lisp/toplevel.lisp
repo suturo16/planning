d17a7324e6c1d750d03bce193617518cbddd3224
Arthur Niedzwiecki <arthur.niedzwiecki@gmail.com>
2017-02-24 11:12:05 +0100

    :muscle: add semrec util

--- plan_execution/lisp/toplevel.lisp
+++ plan_execution/lisp/toplevel.lisp
@@ -46,8 +46,9 @@
   "Use a simple switch to translate a task to a list of designators."
   (alexandria:switch (task :test #'equal)
     ("grasp cylinder"
-     (list (make-designator :action `((:type :grasp) (:arm ,pr2-do::+right-arm+) (:object "cylinder0")))))
+     (list (make-designator :action `((:type :grasp) (:arm ,pr2-do::+right-arm+) (:object "cylinder")))))
     ("cut cake"
-     (list (make-designator :action `((:type :grasp) (:arm ,pr2-do::+right-arm+) (:object "knife0")))
-           (make-designator :action `((:type :cut) (:arm ,pr2-do::+right-arm+) (:knife "knife0") (:cake "box0")))))))
+     (list (make-designator :action `((:type :grasp) (:arm ,pr2-do::+right-arm+) (:object "knife")))
+           (make-designator :action `((:type :cut) (:arm ,pr2-do::+right-arm+) (:knife "knife") (:cake "box")))))))
+
 
