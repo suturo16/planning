06154560c3edafbceb927268a4d1f3e3a259ec95
Christopher Pollok <cpollok@uni-bremen.de>
2017-03-19 22:44:28 +0100

    :muscle: Move communication setup to communication package.

--- plan_execution/lisp/init.lisp
+++ plan_execution/lisp/init.lisp
@@ -1,11 +1,11 @@
 (in-package :plan-execution-package)
 
-(defun init-planning ()
+(defun init-planning (my-ip)
   "Initialize everything planning needs to run."
   (start-ros-node "planning")
 
   ;; Initialize communication with pepper
-  (setup-pepper-communication "192.168.101.97")
+  (pcomm::setup-pepper-communication #'execute my-ip)
   
   ;; Initialize action client
   (pr2-do::setup-move-robot-client)
@@ -14,17 +14,3 @@
   ;;(pr2-do::run-full-pipeline)
 
   (pr2-do::service-run-pipeline "Knife"))
-
-(defun setup-pepper-communication (my-ip &optional (pepper-ip "192.168.101.69") (pepper-port 8000))
-  ;; Initialize the RPC-server
-  (pcomm::init-rpc-server)
-
-  ;; Put Pepper in our *clients* table.
-  (pcomm::|updateObserverClient| pcomm::+pepper-client-id+ pepper-ip pepper-port)
-
-  ;; Tell Pepper where we are.
-  (pcomm::fire-rpc "updateObserverClient" pepper-ip pepper-port
-            pcomm::+pr2-client-id+ my-ip (pcomm::get-local-port))
-
-  ;; Listen to pepper_command topic
-  (pcomm::listen-for-commands #'execute))
