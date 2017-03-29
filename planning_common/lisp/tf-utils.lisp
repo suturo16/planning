73f049a0278a73a702034b3d90163ef15c35d021
Christopher Pollok <cpollok@uni-bremen.de>
2017-03-29 21:25:39 +0200

    :memo: Add docstrings to tf-utils.

--- planning_common/lisp/tf-utils.lisp
+++ planning_common/lisp/tf-utils.lisp
@@ -3,14 +3,20 @@
 (defparameter *transform-listener* nil)
 
 (defun init-transform-listener ()
+  "Initialize `*transform-listener*'."
   (setf *transform-listener* (make-instance 'cl-tf:transform-listener)))
 
 (defun get-transform-listener ()
+  "Return `*transform-listener*'. Initialize it first if necessary."
   (if *transform-listener*
       *transform-listener*
       (init-transform-listener)))
 
 (defun extract-pose-from-transform (parent-frame frame)
+  "Lookup transform between PARENT-FRAME and FRAME and return just the pose.
+
+PARENT-FRAME (string): The source frame in which FRAME is published.
+FRAME (string): The target frame published in relation to PARENT_FRAME."
   (cl-tf:wait-for-transform (get-transform-listener)
                             :source-frame parent-frame
                             :target-frame frame
@@ -23,6 +29,9 @@
      (cl-tf:transform->pose target-transform-stamped)))
 
 (defun tf-pose->string (pose)
+  "Create space-seperated string of the seven values in POSE.
+
+POSE (cl-tf:pose): a pose."
   (let ((origin (cl-tf:origin pose))
         (orientation (cl-tf:orientation pose)))
     (multiple-value-bind (axis angle) (cl-tf:quaternion->axis-angle orientation)
@@ -41,5 +50,7 @@
                 (- 0 normalized-angle))))))
 
 (defun tf-lookup->string (parent-frame frame)
+  "Use `tf-pose->string' and `extract-pose-from-transform' to create a string from a lookup.
+See their documentation for more information."
   (tf-pose->string
    (extract-pose-from-transform parent-frame frame)))
