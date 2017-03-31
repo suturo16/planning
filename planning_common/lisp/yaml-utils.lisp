c432f9befb8264307e1092365541b7aeb477b590
Christopher Pollok <cpollok@uni-bremen.de>
2017-03-31 11:22:34 +0200

    :memo: Add docstrings for yaml-utils.

--- planning_common/lisp/yaml-utils.lisp
+++ planning_common/lisp/yaml-utils.lisp
@@ -1,6 +1,10 @@
 (in-package :planning-common-package)
 
 (defun get-joint-config (config-name)
+  "Return joint configuration CONFIG-NAME as a list.
+
+CONTROLLER-NAME (string): Should be one of the existent controller specifications. These can be found at URL
+`suturo-docs.rtfd.io/en/latest/implementierung/schnittstellen.html#manipulation')."
   (let ((in (open (get-config-yaml-path config-name) :if-does-not-exist nil))
         (out nil))
     (when in
@@ -11,21 +15,30 @@
     (reverse out)))
 
 (defun get-controller-specs (controller-name)
+  "Return controller specification CONTROLLER-NAME as a string.
+
+CONFIG-NAME (string): Should be one of the existent joint configurations. These can be found at URL
+`suturo-docs.rtfd.io/en/latest/implementierung/schnittstellen.html#manipulation')."
   (file->string (get-controller-path controller-name)))
 
 (defun get-controller-yaml-path (controller-name)
+  "Return path to controller specification CONTROLLER-NAME as a string."
   (concatenate 'string
                (get-yaml-path "controller_specs")
                controller-name
                ".yaml"))
 
 (defun get-config-yaml-path (config-name)
+  "Return path to joint configuration CONFIG-NAME as a string."
   (concatenate 'string
                (get-yaml-path "config")
                config-name
                ".yaml"))
 
 (defun get-controller-path (controller-name)
+  "Return path to controller specification CONTROLLER-NAME, without '.yaml' ending, as a string.
+
+For more information see "
   (let* ((file (concatenate 'string
                             (get-yaml-path "controller_specs")
                             controller-name))
@@ -46,6 +59,9 @@
         giskard)))
     
 (defun get-yaml-path (type)
+  "Return path to directory TYPE in ros package 'graspkard' as a string.
+
+TYPE (string): Should be either 'config' or 'controller_specs' but can be anything."
   (concatenate 'string
                (namestring (roslisp::ros-package-path "graspkard"))
                type
