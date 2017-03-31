26b872ba4961990a93ac2d3ec22ae62d8729f851
Christopher Pollok <cpollok@uni-bremen.de>
2017-03-31 14:20:04 +0200

    :memo: Add docstrings in plan_execution.

--- plan_execution/lisp/init.lisp
+++ plan_execution/lisp/init.lisp
@@ -1,7 +1,9 @@
 (in-package :plan-execution-package)
 
 (defun init-planning (my-ip)
-  "Initialize everything planning needs to run."
+  "Initialize everything planning needs to run.
+
+MY-IP (string): IP of the system running planning."
   (start-ros-node "planning")
 
   ;; Initialize communication with pepper
@@ -12,5 +14,4 @@
   
   ;; Let Perception see.
   ;;(pr2-do::run-full-pipeline)
-
   (pr2-do::service-run-pipeline "Knife"))
