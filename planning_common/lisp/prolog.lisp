81fbfca96e723324640c1f796ed4d15c4d068b48
hasu <hawkin@informatik.uni-bremen.de>
2017-04-05 14:32:40 +0200

    Revert ":fix: fixed get-object-info"

--- planning_common/lisp/prolog.lisp
+++ planning_common/lisp/prolog.lisp
@@ -7,8 +7,7 @@
   (cut:lazy-car (json-prolog:prolog
                  `("get_object_infos"
                    ,(format nil "~a~a" +knowrob-iri-prefix+ name)
-                   ?frame ?timestamp ?width ?height ?depth) :lispify T :package :common)))
-
+                      ?frame ?timestamp ?width ?height ?depth) :lispify T :package :pr2-do)))
 
 (defun prolog-seen-since (name frame-id timestamp)
   "Query prolog with 'seen_since(NAME, FRAME_ID, TIMESTAMP)'.
@@ -17,11 +16,11 @@ Return nil otherwise."
   (json-prolog:prolog
                  `("seen_since"
                   ,(format nil "~a~a" +knowrob-iri-prefix+ name)
-                  ,frame-id ,timestamp) :lispify T :package :common))
+                  ,frame-id ,timestamp) :lispify T :package :pr2-do))
 
 (defun prolog-disconnect-frames (parent-frame-id child-frame-id)
   "Query prolog with 'disconnect_frames(PARENT-FRAME-ID, CHILD-FRAME-ID)'.
 Disconnects the frames PARENT-FRAME-ID and CHILD-FRAME-ID."
   (json-prolog:prolog
                  `("disconnect_frames"
-                   ,parent-frame-id ,child-frame-id) :lispify T :package :common))
+                   ,parent-frame-id ,child-frame-id) :lispify T :package :pr2-do))
