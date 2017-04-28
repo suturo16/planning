21c169c83f3efc55ad86893f6ca089e627586fb7
Christopher Pollok <cpollok@uni-bremen.de>
2017-04-28 13:56:57 +0200

    :bug: Add missing exports in pr2-do.

--- pr2_command_pool/lisp/package.lisp
+++ pr2_command_pool/lisp/package.lisp
@@ -1,3 +1,18 @@
 (defpackage :pr2-command-pool-package
   (:nicknames :pr2-do)
-  (:use :cl :roslisp :common))
+  (:use :cl :roslisp :common)
+
+  (:export
+
+   ;; commands
+   :close-gripper
+   :open-gripper
+   :disconnect-obj-from-arm
+   :connect-obj-with-gripper
+   :move-arm-to-object
+   :move-object-with-arm
+   :get-in-base-pose
+   :grasp-knife
+   :detach-knife-from-rack
+   :take-cutting-position
+   :cut-cake))
