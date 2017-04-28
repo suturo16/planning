f4eb01398be79f4924182ac59190ef7c535cb60e
Christopher Pollok <cpollok@uni-bremen.de>
2017-04-28 13:54:26 +0200

    :muscle: Cleanup demo adjustments in plans. Export commands in pr2-command-pool and use single-colon syntax.

--- plan_execution/lisp/plans.lisp
+++ plan_execution/lisp/plans.lisp
@@ -6,68 +6,62 @@
           (progn ,@body)
        (beliefstate:stop-node log-node :success ,T))))
 
-(cram-language:def-cram-function grasp (obj-info arm)
+
+(defun check-object-location (obj-info)
+  "Return t if the object of OBJ-INFO is still at the same location."
+  (when obj-info
+    ;(get-in-base-pose)
+    ;turn head
+    ;(service-run-pipeline)
+    (when (seen-since obj-info)
+      T)))
+
+
+(cpl:def-cram-function grasp (obj-info arm)
   "Grasp the object described by OBJECT-INFO with ARM.\
 
 OBJ-INFO (obj-info): Description of the target object.
 ARM (string): Which arm to use. Use one of the constants defined in planning-common."
-  (ros-info "grasp" "Starting to grasp object ~a with arm ~a."
-            (pr2-do::object-info-name obj-info)
+  (ros-info (grasp) "Starting to grasp object ~a with arm ~a."
+            (common:object-info-name obj-info)
             arm)
-  ;;(if (pr2-do::check-object-location obj-info)
-      ;; grasp it
+  (if (check-object-location obj-info)
+      ;; if object found, grasp it
       (with-logging-node "GRASP-OBJECT"
-        (beliefstate::annotate-resource "objectInfo" (pr2-do::object-info-name obj-info) "knowrob")
+        (beliefstate::annotate-resource "objectInfo" (common:object-info-name obj-info) "knowrob")
         (beliefstate::annotate-resource "arm" arm "knowrob")
         ;; grasp it
         (seq
-          (alexandria:switch ((pr2-do::object-info-name obj-info) :test #'equal)
+          (alexandria:switch ((common:object-info-name obj-info) :test #'equal)
             ("Knife" (grasp-knife obj-info arm))
             ("Cylinder" (grasp-object obj-info arm)))
-          (pr2-do::connect-obj-with-gripper obj-info arm)
-          (ros-info "grasp" "Connected object ~a with arm ~a."
-                    (pr2-do::object-info-name obj-info)
+          (pr2-do:connect-obj-with-gripper obj-info arm)
+          (ros-info (grasp) "Connected object ~a with arm ~a."
+                    (common:object-info-name obj-info)
                     arm)))
       ;; else complain
-      ;;(ros-error "grasp" "Object ~a not found" (pr2-do::object-info-name obj-info)))
-  )
+      (ros-error "grasp" "Object ~a not found" (common:object-info-name obj-info))))
   
-
 
 (defun grasp-knife (knife-info arm)
   "Grasp the knife described by KNIFE-INFO with ARM."
-;; (pr2-do::grasp-knife knife-info arm)
-;; (pr2-do::close-gripper arm 100)
-  (ms2-grasp-knife knife-info arm))
+  (pr2-do:grasp-knife knife-info arm)
+  (ros-info (grasp knife) "Close gripper.")
+  (pr2-do:close-gripper arm 100))
 
-(defun ms2-grasp-knife (knife-info arm)
-  (ros-info "ms2-grasp-knife" "connect Knife frame to ododm.")
-  (pr2-do::service-connect-frames "/odom_combined"  "/Knife")
-  (ros-info "ms2-grasp-knife" "grasp the knife.")
-  (pr2-do::grasp-knife knife-info arm)
-  (ros-info "ms2-grasp-knife" "close-gripper")
-  (pr2-do::close-gripper arm 100)
-  (ros-info "ms2-grasp-knife" "reconnect frames from odom to arm")
- 
- ;; (pr2-do::detach-knife-from-rack (pr2-do::get-object-info "Knife") pr2-do::+right-arm+)
-  (pr2-do::prolog-disconnect-frames "/odom_combined" "/Knife")
-  (pr2-do::service-connect-frames "/r_wrist_roll_link" "/Knife")
-  (pr2-do::get-in-base-pose)
-  ;;TODO: test detach, add it in here. 
-  )
 
 (defun grasp-object (obj-info arm)
   "Grasp the object described by OBJECT-INFO with ARM."
-  (ros-info "grasp-object" "Open gripper")
-  (pr2-do::open-gripper arm)
-  (ros-info "grasp-object" "Move arm to object ~a." (pr2-do::object-info-name obj-info))
-  (pr2-do::move-arm-to-object obj-info arm)
-  (ros-info "grasp-object" "Close gripper.")
-  (pr2-do::close-gripper arm 50)
-  (ros-info "grasp-object" "Done."))
+  (ros-info (grasp object) "Open gripper")
+  (pr2-do:open-gripper arm)
+  (ros-info (grasp object) "Move arm to object ~a." (common:object-info-name obj-info))
+  (pr2-do:move-arm-to-object obj-info arm)
+  (ros-info (grasp object) "Close gripper.")
+  (pr2-do:close-gripper arm 50)
+  (ros-info (grasp object) "Done."))
 
 
-(cram-language:def-cram-function place-object (obj-info loc-info arm)
+(cpl:def-cram-function place-object (obj-info loc-info arm)
   "Place object described by OBJECT-INFO at location described by LOCATION-INFO with ARM.
 Assuming the object is placed on ARM's gripper.
 
@@ -75,63 +69,80 @@ OBJ-INFO (common:obj-info): Description of the target object.
 LOC-INFO (common:obj-info): Description of target location.
 ARM (string): Which arm to use. Use one of the constants defined in planning-common."
   (with-logging-node "PLACE-OBJECT"
-    (beliefstate::annotate-resource "objectInfo" (pr2-do::object-info-name obj-info) "knowrob")
+    (beliefstate::annotate-resource "objectInfo" (common:object-info-name obj-info) "knowrob")
     (beliefstate::annotate-resource "locationInfo" loc-info "knowrob")
     (beliefstate::annotate-resource "arm" arm "knowrob")
-    (ros-info "place-object" "Move object with arm.")
-    (pr2-do::move-object-with-arm loc-info obj-info arm)
-    (ros-info "place-object" "Open gripper.")
-    (pr2-do::open-gripper arm)
-    (ros-info "place-object" "Done.")))
+    (ros-info (place-object) "Move object with arm.")
+    (pr2-do:move-object-with-arm loc-info obj-info arm)
+    (ros-info (place-object) "Open gripper.")
+    (pr2-do:open-gripper arm)
+    (ros-info (place-object) "Done.")))
 
 
-(cram-language:def-cram-function detach-object-from-rack (obj-info arm)
+(cpl:def-cram-function detach-object-from-rack (obj-info arm)
   "Detach object described by OBJECT-INFO from the rack with ARM.
 Assume object is attached to the rack.
 
 OBJ-INFO (obj-info): Description of the target object.
 ARM (string): Which arm to use. Use one of the constants defined in planning-common."
   ;; this doesn't work, I think the axes are wrong since bot moves his torso down
-  (pr2-do::detach-knife-from-rack obj-info arm)
-  ;;(pr2-do::get-in-base-pose)
-  )
+  (ros-info (detach-object-from-rack) "Detaching.")
+  (pr2-do:detach-knife-from-rack obj-info arm)
+  (ros-info (detach-object-from-rack) "Done."))
   
 
-(cram-language:def-cram-function cut-object (arm knife-info cake-info)
+(cpl:def-cram-function cut-object (arm knife-info cake-info)
   "Cut object described by CAKE-INFO with knife described by KNIFE-INFO using ARM.
 Assume the knife is in the gripper of ARM.
 
 CAKE-INFO (obj-info): Description of the target object.
 KNIFE-INFO (obj-info): Description of the cutting tool.
 ARM (string): Which arm to use. Use one of the constants defined in planning-common."
- ;;(if (pr2-do::check-object-location cake-info)
+ (if (check-object-location cake-info)
       ;; if object is found, cut it
       (with-logging-node "CUT-OBJECT"
-        (beliefstate::annotate-resource "knifeInfo" (pr2-do::object-info-name knife-info) "knowrob")
-        (beliefstate::annotate-resource "cakeInfo" (pr2-do::object-info-name cake-info) "knowrob")
+        (beliefstate::annotate-resource "knifeInfo" (common:object-info-name knife-info) "knowrob")
+        (beliefstate::annotate-resource "cakeInfo" (common:object-info-name cake-info) "knowrob")
         (beliefstate::annotate-resource "arm" arm "knowrob")
-        (pr2-do::service-connect-frames "/odom_combined" "/Box")
-        (ros-info "-cut-object" "Getting into cutting position.")
-        (pr2-do::take-cutting-position cake-info knife-info arm 0.01)
-        (ros-info "cut-object" "Cutting.")
-        (pr2-do::cut-cake cake-info knife-info arm 0.01)
+        (common:service-connect-frames "/odom_combined" "/Box")
+        (ros-info (cut-object) "Getting into cutting position.")
+        (pr2-do:take-cutting-position cake-info knife-info arm 0.01)
+        (ros-info (cut-object) "Cutting.")
+        (pr2-do:cut-cake cake-info knife-info arm 0.01)
         ; TODO(cpo): get cake-piece-info
         ; (pr2-do::push-aside cake-info cake-piece-info)
-        (ros-info "cut-object" "Done."))
+        (ros-info (cut-object) "Done."))
       ;; else complain
-      ;;(ros-error "cut-object" "Cannot find object '~a', which I am supposed to cut."
-                 ;;(pr2-do::object-info-name cake-info)))
+      (ros-error (cut-object) "Cannot find object '~a', which I am supposed to cut."
+                 (common:object-info-name cake-info))))
+
+
+(defun ms2-grasp-knife (knife-info arm)
+  (ros-info (ms2-grasp-knife) "connect Knife frame to ododm.")
+  (common:service-connect-frames "/odom_combined"  "/Knife")
+  (ros-info (ms2-grasp-knife) "grasp the knife.")
+  (pr2-do:grasp-knife knife-info arm)
+  (ros-info (ms2-grasp-knife) "close-gripper")
+  (pr2-do:close-gripper arm 100)
+  ;; (pr2-do::detach-knife-from-rack (pr2-do::get-object-info "Knife") pr2-do::+right-arm+)
+  (ros-info (ms2-grasp-knife) "reconnect frames from odom to arm")
+  (common:prolog-disconnect-frames "/odom_combined" "/Knife")
+  (common:service-connect-frames "/r_wrist_roll_link" "/Knife")
+  (pr2-do:get-in-base-pose)
+  ;;TODO: test detach, add it in here.
   )
 
+
 (defun ms2-cut-cake (cake-info knife-info arm)
-  (pr2-do::service-connect-frames "/odom_combined" "/Box")
+  (common:service-connect-frames "/odom_combined" "/Box")
   (ros-info "ms2-cut-object" "Getting into cutting position.")
-  (pr2-do::take-cutting-position cake-info knife-info arm 0.01)
+  (pr2-do:take-cutting-position cake-info knife-info arm 0.01)
   (ros-info "ms2-cut-object" "Cutting.")
-  (pr2-do::cut-cake cake-info knife-info arm 0.01)
+  (pr2-do:cut-cake cake-info knife-info arm 0.01)
   (ros-info "ms2-cut-cake" "Done."))
 
+
 (defun ms2-full-demo ()
-  (plan-execution-package::ms2-grasp-knife (common:get-object-info "Knife") pr2-do::+right-arm+ )
-  (pr2-do::get-in-base-pose)
-  (plan-execution-package::ms2-cut-cake (pr2-do::get-object-info "Box") (pr2-do::get-object-info "Knife") pr2-do::+right-arm+))
+  (ms2-grasp-knife (common:get-object-info "Knife") common:+right-arm+ )
+  (pr2-do:get-in-base-pose)
+  (ms2-cut-cake (common:get-object-info "Box") (common:get-object-info "Knife") common:+right-arm+))
