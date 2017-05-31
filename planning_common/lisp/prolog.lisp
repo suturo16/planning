56e6e83304597dbf9b009d6ca04a6e5d893de8f2
hasu <hawkin@informatik.uni-bremen.de>
2017-05-31 15:39:18 +0200

    :unicorn: added connect_frames via prolog

--- planning_common/lisp/prolog.lisp
+++ planning_common/lisp/prolog.lisp
@@ -32,3 +32,8 @@ Disconnects the frames PARENT-FRAME-ID and CHILD-FRAME-ID."
   (json-prolog:prolog
                  `("disconnect_frames"
                    ,parent-frame-id ,child-frame-id) :lispify T :package :common))
+(defun prolog-connect-frames (parent-frame-id child-frame-id)
+  "connect frames through prolog."
+  (json-prolog:prolog
+                 `("connect_frames"
+                   ,parent-frame-id ,child-frame-id) :lispify T :package :common))
