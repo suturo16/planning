fa474d011f199d8f9d277dbe49cdf210cb584edc
Arthur Niedzwiecki <arthur.niedzwiecki@gmail.com>
2017-06-02 16:36:34 +0200

    :muscle: Use current controller setup from branch feature/ms4-plans

--- pr2_command_pool/lisp/commands.lisp
+++ pr2_command_pool/lisp/commands.lisp
@@ -26,10 +26,10 @@
     (action-move-robot (format nil "pr2_upper_body_~a_arm" arm-str)
                        (format nil "pr2_grasp_control_~a" arm)
                        (lambda (v) (< v 0.01))
-                       (make-param +transform+ nil "object_frame"
+                       (make-param +transform+ nil "cylinder_frame"
                                    (format nil "~a ~a" (object-info-name obj-info) "base_link"))
-                       (make-param +double+ T "object_width" (write-to-string (object-info-width obj-info)))
-                       (make-param +double+ T "object_height" (write-to-string (object-info-height obj-info))))))
+                       (make-param +double+ T "cylinder_height" (write-to-string (object-info-width obj-info)))
+                       (make-param +double+ T "cylinder_width" (write-to-string (object-info-height obj-info))))))
 
 (defun move-object-with-arm (loc-info obj-info arm)
   "Call action to place the object of OBJ-INFO at the location of LOC-INFO.
@@ -38,12 +38,12 @@ Assume that the object is attached to ARM."
     (action-move-robot (format nil "pr2_upper_body_~a_arm" arm-str)
                        (format nil "pr2_place_control_~a" arm)
                        (lambda (v) (< v 0.01))
-                       (make-param +transform+ NIL "location_frame"
+                       (make-param +transform+ NIL "target_frame"
                                    (format nil "~a ~a" (object-info-name loc-info) "base_link"))
-                       (make-param +transform+ NIL "object_frame"
+                       (make-param +transform+ NIL "cylinder_in_gripper"
                                    (format nil "~a ~a" (object-info-name obj-info) (format nil "~a_wrist_roll_link" arm)))
-                       (make-param +double+ T "object_width" (write-to-string (object-info-width obj-info)))
-                       (make-param +double+ T "object_height" (write-to-string (object-info-height obj-info)))
+                       (make-param +double+ T "cylinder_width" (write-to-string (object-info-width obj-info)))
+                       (make-param +double+ T "cylinder_height" (write-to-string (object-info-height obj-info)))
                        (make-param +double+ T (format nil "~a_gripper_effort" arm) (write-to-string 50)))))
 
 
@@ -55,33 +55,33 @@ Assume that the object is attached to ARM."
   "Call action to move the object on the tool of TOOL-INFO above the lcoation of LOC-INFO and flip the tool to place the obejct at the location."
   (let ((arm-str (if (string= arm +left-arm+) "left" "right")))
     (action-move-robot (format nil "pr2_upper_body_~a_arm" arm-str)
-                       (format nil "pr2_place_control_~a" arm)
+                       (format nil "pr2_move_and_flip_r")
                        (lambda (v) (< v 0.01))
-                       (make-param +transform+ NIL "tool_frame"
+                       (make-param +transform+ NIL "spatula_in_gripper"
                                    (format nil "~a ~a" (object-info-name tool-info) (format nil  "~a_wrist_roll_link" arm)))
-                       (make-param +transform+ NIL "location_frame"
+                       (make-param +transform+ NIL "goal_frame"
                                    (format nil "~a ~a" (object-info-name loc-info) "base_link"))
-                       (make-param +double+ T "tool_width" (write-to-string +tool-width+))
-                       (make-param +double+ T "loc-radius" (write-to-string +loc-radius+)))))
+                       (make-param +double+ T "spatula_radius" (write-to-string +tool-width+))
+                       (make-param +double+ T "goal_radius" (write-to-string +loc-radius+)))))
 
 (defun get-in-base-pose ()
   "Bring PR2 into base (mantis) pose."
   (action-move-robot "pr2_upper_body" "pr2_upper_body_joint_control" (lambda (v) (< v 0.05))
-                     (make-param +double+ T "torso_lift_joint" "0.25")
-                     (make-param +double+ T "l_shoulder_pan_joint" "1.23679")
-                     (make-param +double+ T "l_shoulder_lift_joint" "-0.247593")
-                     (make-param +double+ T "l_upper_arm_roll_joint" "0.614271")
-                     (make-param +double+ T "l_elbow_flex_joint" "-1.38094")
-                     (make-param +double+ T "l_forearm_roll_joint" "-4.94757")
-                     (make-param +double+ T "l_wrist_flex_joint" "-1.56861")
-                     (make-param +double+ T "l_wrist_roll_joint" "0")
-                     (make-param +double+ T "r_shoulder_pan_joint" "-1.23679")
-                     (make-param +double+ T "r_shoulder_lift_joint" "-0.247593")
-                     (make-param +double+ T "r_upper_arm_roll_joint" "-0.614271")
-                     (make-param +double+ T "r_elbow_flex_joint" "-1.38094")
-                     (make-param +double+ T "r_forearm_roll_joint" "4.94757")
-                     (make-param +double+ T "r_wrist_flex_joint" "-1.56861")
-                     (make-param +double+ T "r_wrist_roll_joint" "0")))
+                     (make-param +double+ T "torso_lift_joint_goal" "0.25")
+                     (make-param +double+ T "l_shoulder_pan_joint_goal" "1.23679")
+                     (make-param +double+ T "l_shoulder_lift_joint_goal" "-0.247593")
+                     (make-param +double+ T "l_upper_arm_roll_joint_goal" "0.614271")
+                     (make-param +double+ T "l_elbow_flex_joint_goal" "-1.38094")
+                     (make-param +double+ T "l_forearm_roll_joint_goal" "-4.94757")
+                     (make-param +double+ T "l_wrist_flex_joint_goal" "-1.56861")
+                     (make-param +double+ T "l_wrist_roll_joint_goal" "0")
+                     (make-param +double+ T "r_shoulder_pan_joint_goal" "-1.23679")
+                     (make-param +double+ T "r_shoulder_lift_joint_goal" "-0.247593")
+                     (make-param +double+ T "r_upper_arm_roll_joint_goal" "-0.614271")
+                     (make-param +double+ T "r_elbow_flex_joint_goal" "-1.38094")
+                     (make-param +double+ T "r_forearm_roll_joint_goal" "4.94757")
+                     (make-param +double+ T "r_wrist_flex_joint_goal" "-1.56861")
+                     (make-param +double+ T "r_wrist_roll_joint_goal" "0")))
 
 
 
@@ -122,10 +122,10 @@ Assume that the object is attached to ARM."
     (action-move-robot (format nil "pr2_upper_body_~a_arm" arm-str)
                        (format nil "pr2_grasp_plate_~a" arm)
                        (lambda (v) (< v 0.025))
-                       (make-param +transform+ NIL "edge_center_frame" (format nil "~a ~a" (object-info-name plate-info) "base_link"))
+                       (make-param +transform+ NIL "plate_frame" (format nil "~a ~a" (object-info-name plate-info) "base_link"))
                        (make-param +double+ T "edge_radius" (write-to-string +edge-radius+))
-                       (make-param +double+ T "edge_height" (write-to-string +edge-height+))
-                       (make-param +double+ T "edge_width" (write-to-string +edge-width+))
+                       (make-param +double+ T "edge_z" (write-to-string +edge-height+))
+                       (make-param +double+ T "edge_depth" (write-to-string +edge-width+))
                        (make-param +double+ T "edge_angle" (write-to-string +edge-angle+)))))
 
 ;; spatula constants
@@ -142,7 +142,7 @@ Assume that the object is attached to ARM."
     (action-move-robot (format nil "pr2_upper_body_~a_arm" arm-str)
                        (format nil "pr2_grasp_fingerHandle_~a" arm)
                        (lambda (v) (< v 0.025))
-                       (make-param +transform+ NIL "spatula_handle_tf" (format nil "~a ~a" "spatula_handle" "base_link"))
+                       (make-param +transform+ NIL "handle_frame" (format nil "~a ~a" "spatula_handle" "base_link"))
                        (make-param +double+ T "handle_depth" (write-to-string +spatula-handle-depth+))
                        (make-param +double+ T "handle_width" (write-to-string +spatula-handle-width+)))))
 
@@ -152,8 +152,8 @@ Assume that the object is attached to ARM."
     (action-move-robot (format nil "pr2_upper_body_~a_arm" arm-str)
                        (format nil "pr2_release_~a" arm)
                        (lambda (v) (< v 0.03))
-                       (make-param +transform+ T "gripper_frame" (tf-lookup->string "base_link" (format nil "~a_wrist_roll_link" arm)))
-                       (make-param +double+ NIL "gripper-width" gripper-width))))
+                       (make-param +transform+ T "start_pose" (tf-lookup->string "base_link" (format nil "~a_wrist_roll_link" arm)))
+                       (make-param +double+ NIL "gripper_opening" gripper-width))))
 
 (defun detach-knife-from-rack (knife-info arm)
   "Call action to move the knife of KNIFE-INFO away from the rack.
@@ -162,9 +162,9 @@ Assume that the knife is connected to ARM."
     (action-move-robot (format nil "pr2_upper_body_~a_arm" arm-str)
                        (format nil "pr2_detach_knife_~a" arm)
                        (lambda (v) (< v 0.000001))
-                       (make-param +transform+ NIL "knife_frame" (format nil  "~a ~a" (object-info-name knife-info)
+                       (make-param +transform+ NIL "knife_frame_wrist" (format nil  "~a ~a" (object-info-name knife-info)
                                                                  (format nil "~a_wrist_roll_link" arm)))
-                       (make-param +transform+ T "original_knife_tf" (tf-lookup->string "base_link" (object-info-name knife-info))))))
+                       (make-param +transform+ T "rack_frame" (tf-lookup->string "base_link" (object-info-name knife-info))))))
 
 (defun take-cutting-position (cake-info knife-info arm slice-width)
   "Call action to take a position ready to cut the cake of CAKE-INFO.
@@ -175,12 +175,12 @@ for cutting with SLICE-WIDTH."
     (action-move-robot (format nil "pr2_upper_body_~a_arm" arm-str)
                        (format nil "pr2_cut_position_~a" arm)
                        (lambda (v) (< v 0.01))
-                       (make-param +transform+ NIL "cake_tf" (format nil "~a ~a" (object-info-name cake-info) "base_link"))
-                       (make-param +double+ T "cake_length_x" (write-to-string (object-info-depth cake-info)))
-                       (make-param +double+ T "cake_width_y" (write-to-string (object-info-width cake-info)))
-                       (make-param +double+ T "cake_height_z" (write-to-string (object-info-height cake-info)))
-                       (make-param +transform+ NIL "knife_tf" (format nil "~a ~a" (object-info-name knife-info) (format nil "~a_wrist_roll_link" arm)))
-                       (make-param +double+ T "blade_height" (write-to-string +blade-height+))  ; (write-to-string (object-info-height knife-info)))
+                       (make-param +transform+ NIL "cake_frame" (format nil "~a ~a" (object-info-name cake-info) "base_link"))
+                       (make-param +double+ T "cake_length" (write-to-string (object-info-depth cake-info)))
+                       (make-param +double+ T "cake_width" (write-to-string (object-info-width cake-info)))
+                       (make-param +double+ T "cake_height" (write-to-string (object-info-height cake-info)))
+                       (make-param +transform+ NIL "knife_ee_frame" (format nil "~a ~a" (object-info-name knife-info) (format nil "~a_wrist_roll_link" arm)))
+                       (make-param +double+ T "knife_height" (write-to-string +blade-height+))  ; (write-to-string (object-info-height knife-info)))
                        (make-param +double+ T "handle_length" (write-to-string +handle-length+))
                        (make-param +double+ T "slice_width" (write-to-string slice-width)))))
 
@@ -193,24 +193,24 @@ to cut pieces with SLICE-WIDTH."
     (action-move-robot (format nil "pr2_upper_body_~a_arm" arm-str)
                        (format nil "pr2_cut_~a" arm)
                        (lambda (v) (< v 0.01))
-                       (make-param +transform+ NIL "cake_tf" (format nil "~a ~a" (object-info-name cake-info) "base_link"))
-                       (make-param +double+ T "cake_length_x" (write-to-string (object-info-depth cake-info)))
-                       (make-param +double+ T "cake_width_y" (write-to-string (object-info-width cake-info)))
-                       (make-param +double+ T "cake_height_z" (write-to-string (object-info-height cake-info)))
-                       (make-param +transform+ NIL "knife_tf" (format nil "~a ~a" (object-info-name knife-info) (format nil "~a_wrist_roll_link" arm)))
-                       (make-param +double+ T "blade_height" (write-to-string +blade-height+)) ; (write-to-string (object-info-height knife-info)))
-                       (make-param +double+ T "handle_length" (write-to-string +handle-length+))
+                       (make-param +transform+ NIL "cake_frame" (format nil "~a ~a" (object-info-name cake-info) "base_link"))
+                       (make-param +double+ T "cake_length" (write-to-string (object-info-depth cake-info)))
+                       (make-param +double+ T "cake_width" (write-to-string (object-info-width cake-info)))
+                       (make-param +double+ T "cake_height" (write-to-string (object-info-height cake-info)))
+                       (make-param +transform+ NIL "knife_frame" (format nil "~a ~a" (object-info-name knife-info) (format nil "~a_wrist_roll_link" arm)))
+                       (make-param +double+ T "knife_height" (write-to-string +blade-height+)) ; (write-to-string (object-info-height knife-info)))
+                       ;;(make-param +double+ T "handle_length" (write-to-string +handle-length+))
                        (make-param +double+ T "slice_width" (write-to-string slice-width)))))
 
 
 (defun move-slice-aside (knife-info cake-info target-info arm)
   (let ((arm-str (if (string= arm +left-arm+) "left" "right")))
     (action-move-robot (format nil "pr2_upper_body_~a_arm" arm-str)
-                       (format nil "pr2_move_slice_~a" arm)
+                       (format nil "pr2_push_cake_~a" arm)
                        (lambda (v) (< v 0.01))
-                       (make-param +transform+ NIL "knife_tf" (format nil "~a ~a_wrist_roll_link" (object-info-name knife-info) arm))
-                       (make-param +transform+ NIL "cake_tf" (format nil "~a base_link" (object-info-name cake-info)))
-                       (make-param +transform+ NIL "target_tf" (format nil "~a base_link" (object-info-name target-info))))))
+                       (make-param +transform+ NIL "knife_in_wrist" (format nil "~a ~a_wrist_roll_link" (object-info-name knife-info) arm))
+                       (make-param +transform+ NIL "cake_frame" (format nil "~a base_link" (object-info-name cake-info))))))
+                       ;;(make-param +transform+ NIL "target_tf" (format nil "~a base_link" (object-info-name target-info))))))
 
 
 (defun look-at (obj-info)
@@ -218,4 +218,4 @@ to cut pieces with SLICE-WIDTH."
     (action-move-robot (format nil "pr2_lookAt_joints")
                        (format nil "pr2_look_at")
                        (lambda (v) (< v 0.01))
-                       (make-param +transform+ NIL "obj_frame" (format nil "~a ~a" (object-info-name obj-info) "base_link"))))
+                       (make-param +transform+ NIL "focal_point" (format nil "~a ~a" (object-info-name obj-info) "base_link"))))
