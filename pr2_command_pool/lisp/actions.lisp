b04cb4c5bde53c36f164647abea50304ebea0168
hasu <hawkin@informatik.uni-bremen.de>
2017-06-13 17:56:18 +0200

    :fix: adjustment of values for grasp, take cutting position

--- pr2_command_pool/lisp/actions.lisp
+++ pr2_command_pool/lisp/actions.lisp
@@ -9,6 +9,6 @@
     (common::action-move-robot
      (format nil "pr2_~a_gripper" arm-str)
      (format nil "gripper_control_~a" arm)
-     (lambda (v) (< v 0.005))
+     (lambda (v) (< v 0.017))
      (make-param +double+ T "gripper_goal" (write-to-string target-width))
      (make-param +double+ T effort-param-name (write-to-string strength)))))
