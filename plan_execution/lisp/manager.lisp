0ccc36121afce4caff11e069fdc46afc7c818369
Christopher Pollok <cpollok@uni-bremen.de>
2017-07-28 14:45:23 +0200

    :muscle: Current version of manager.

--- plan_execution/lisp/manager.lisp
+++ plan_execution/lisp/manager.lisp
@@ -5,8 +5,11 @@
 (defparameter *current-count* 0)
 
 (defun next-guest-id ()
-  "Set the current guest id to the lowest unserved guest."
-  (setf *current-guest-id* (car (common:get-guest-ids))))
+  "Set *current-guest-id* to the next unserved guest."
+  (loop
+    while (not *current-guest-id*)
+    do (setf *current-guest-id* (common:get-current-order))
+    unless *current-guest-id* do (sleep 1)))
 
 (defun get-order ()
   (common:get-guest-order *current-guest-id*))
@@ -17,18 +20,21 @@
 
 (defun do-order ()
   (execute "cut")
-  ;; increase order
+  (common:prolog-increase-delivered-amount 1)
   (setf *last-phase* :cut))
 
 (defun finish-order ()
-  (setf *current-guest-id* NIL)
-  (setf *current-count* 0)
   (execute "deliver")
+  ;; (notify-order-ready *current-guest-id*)
+  (setf *current-guest-id* NIL)
   (setf *last-phase* :deliver))
 
 (defun start-caterros ()
   (loop
     unless *current-guest-id* do (next-guest-id)
-      unless *current-guest-id* return nil
-        when (< *current-count* (get-order)) do (do-order)
-          when (>= *current-count* (get-order)) do (finish-order)))
+      unless (member *last-phase* '(:prep :cut)) do (prep)
+        when (not (= (common:get-remaining-amount-for-order *current-guest-id*) 0)) do (do-order)
+          when (<= (common:get-remaining-amount-for-order *current-guest-id*) 0) do (finish-order)))
+
+(defun test-guest ()
+  (common::prolog-assert-dialog-element "{guestId:\"1\",query:{type:\"setCake\",amount:2,guestName:\"arthur\"}}"))
