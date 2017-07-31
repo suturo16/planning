925cbfbe343427b8609a96bd23727b566cd46764
jasmin <jasmin@henzenet.de>
2017-07-31 16:15:12 +0200

    :unicorn: Integrate problem and plan generation

--- plan_generator/lisp/plan-generator.lisp
+++ plan_generator/lisp/plan-generator.lisp
@@ -1,5 +1,64 @@
 (in-package :plan-generator-package)
 
-(defun generate-plan-on-goal (goal)
-  "This should start the generaton of the plan upon the given goal."
-  goal)
+"Objects available in the problem scenario."
+(defvar objects '("knife - knife" "spatula - spatula" "plate - plate" "cake - cake"))
+
+"Predicates describing the init state of the problem scenario."
+(defvar init-predicates '("free left" "free right" "at-rack knife" "on-table cake" "on-table plate" "on-table spatula" "empty spatula"))
+
+"Predicates describing the goal state of the problem scenario."
+(defvar goal-predicates '("on-turtlebot plate"))
+
+"Name of the domain to that the problem scenario belongs to."
+(defvar domain "caterros")
+
+"Name of the problem."
+(defvar name "cake-on-turtlebot")
+
+"Path to the folder where the generated file should be saved."
+(defvar path "/home/jasmin/suturo_ws/src/planning/plan_generator/pddl/task.pddl")
+
+
+(defun generate-plan-for-cake-serving (&optional (n 1))
+"Generate a plan for serving N pieces of cake."
+	(create-pddl-file-for-cake-serving n)
+	(service-generate-plan "domain.pddl" "task.pddl"))
+
+
+
+(defun create-pddl-file-for-cake-serving (&optional (n 1))
+"Create pddl file for the serving of pieces of cake.
+Optionally specify amount N, default is 1."
+	(generate-pddl-problem 
+		name 
+		domain 
+		(add-pieces-of-cake n objects)
+		(add-init-predicates-for-pieces-of-cake n init-predicates)
+		(add-goal-predicates-for-pieces-of-cake n goal-predicates)
+		path
+))
+
+
+(defun add-pieces-of-cake (n objects)
+"Add N pieces-of-cake to a list of OBJECTS."
+ 	(append 
+		objects 
+		(loop for i to (- n 1) 
+			collect (concatenate 'string "pieceofcake" (write-to-string i) " - pieceofcake"))))
+
+
+(defun add-init-predicates-for-pieces-of-cake (n predicates)
+"Add init-predicates for N pieces-of-cake to a list of PREDICATES."
+	(append 
+		predicates 
+		(loop for i to (- n 1) 
+			collect (concatenate 'string "not-existing pieceofcake" (write-to-string i)))))
+
+
+(defun add-goal-predicates-for-pieces-of-cake (n predicates)
+"Add goal-predicates for N pieces-of-cake to a list of PREDICATES."
+	(append 
+		(loop for i to (- n 1) 
+			collect (concatenate 'string "on-plate pieceofcake" (write-to-string i) " plate"))
+		predicates))
+
--- plan_generator/pddl/task.pddl
+++ plan_generator/pddl/task.pddl
@@ -7,7 +7,10 @@
     plate - plate
     cake - cake
     pieceofcake0 - pieceofcake
-    pieceofcake1 - pieceofcake)
+    pieceofcake1 - pieceofcake
+    pieceofcake2 - pieceofcake
+    pieceofcake3 - pieceofcake
+    pieceofcake4 - pieceofcake)
 
   (:init 
     (free left)
@@ -18,10 +21,16 @@
     (on-table spatula)
     (empty spatula)
     (not-existing pieceofcake0)
-    (not-existing pieceofcake1))
+    (not-existing pieceofcake1)
+    (not-existing pieceofcake2)
+    (not-existing pieceofcake3)
+    (not-existing pieceofcake4))
 
   (:goal
     (and 
         (on-plate pieceofcake0 plate)
         (on-plate pieceofcake1 plate)
+        (on-plate pieceofcake2 plate)
+        (on-plate pieceofcake3 plate)
+        (on-plate pieceofcake4 plate)
         (on-turtlebot plate))))
