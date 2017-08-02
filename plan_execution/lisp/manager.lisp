f749fdf54c48bad0e47108be749eca142f530a10
Christopher Pollok <cpollok@uni-bremen.de>
2017-08-02 15:35:53 +0200

    :muscle: Use plan generator in manager.

--- plan_execution/lisp/manager.lisp
+++ plan_execution/lisp/manager.lisp
@@ -29,12 +29,30 @@
   (setf *current-guest-id* NIL)
   (setf *last-phase* :deliver))
 
-(defun start-caterros ()
+(defun use-generator ()
+  "Use the plan generator to generate a plan for serving the desired amount of cake slices."
+  (ros-info (plan-manager) "Using plan generator to plan to serve cake.")
+  (execute-desigs
+   (transform-plan-to-action-designators
+    (pgeneration::generate-plan-for-cake-serving
+     (common:get-remaining-amount-for-order *current-guest-id*)))))
+
+(defun start-caterros (&optional (use-generator NIL))
   (loop
-    unless *current-guest-id* do (next-guest-id)
-      unless (member *last-phase* '(:prep :cut)) do (prep)
-        when (not (= (common:get-remaining-amount-for-order *current-guest-id*) 0)) do (do-order)
-          when (<= (common:get-remaining-amount-for-order *current-guest-id*) 0) do (finish-order)))
+    unless *current-guest-id*
+      do (next-guest-id)
+    end
+    if use-generator
+      do (use-generator) 
+    else
+      unless (member *last-phase* '(:prep :cut))
+        do (prep)
+        and
+          when (not (= (common:get-remaining-amount-for-order *current-guest-id*) 0))
+            do (do-order)
+            and
+              when (<= (common:get-remaining-amount-for-order *current-guest-id*) 0)
+                do (finish-order)))
 
 (defun test-guest ()
   (if (common::prolog-get-customer-infos 1)
@@ -42,8 +60,8 @@
       (common::prolog-assert-dialog-element "{guestId:\"1\",query:{type:\"setCake\",amount:2,guestName:\"arthur\"}}")))
 
 (defun transform-plan-to-action-designators (plan)
-"Transform PLAN to list of action designators."
- (map 'cons (lambda (task) (make-designator :action
-               (map 'cons
-                    (lambda (x) (cons (intern (string-upcase (first x))) (cdr x)))
-                    task))) (yason:parse plan :object-as :alist)))
+  "Transform PLAN to list of action designators."
+  (map 'cons (lambda (task) (make-designator :action
+                                             (map 'cons
+                                                  (lambda (x) (cons (intern (string-upcase (first x))) (cdr x)))
+                                                  task))) (yason:parse plan :object-as :alist)))
